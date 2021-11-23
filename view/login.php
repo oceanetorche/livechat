dfef07d2d92723ba00dae230da0c0e8422cd89f9
Oceane.PICHONNAZ <Oceane.PICHONNAZ@SC-C232-PC08>
2021-11-23 16:24:39 +0100

    sign up page

--- view/login.php
+++ view/login.php
@@ -1,8 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<!-- content page -->
+<section class="bgwhite p-t-66 p-b-60">
+    <div class="container">
+        <div class="row">
 
-/**
- * @file      login.php
- * @brief     File description
- * @author    Created by Henry Burgat & Océane Torche
- * @version   23.11.2021
- */
+            <div class="col-md-12 p-b-30">
+                <form class="leave-comment">
+                    <h4 class="m-text26 p-b-36 p-t-15">
+                        Connectez-vous
+                    </h4>
+
+                    <div class="bo4 of-hidden size15 m-b-20">
+                        <input class="sizefull s-text7 p-l-22 p-r-22" type="email" name="email" placeholder="Adresse email">
+                    </div>
+
+                    <div class="bo4 of-hidden size15 m-b-20">
+                        <input class="sizefull s-text7 p-l-22 p-r-22" type="password" name="userPswd" placeholder="Mot de passe">
+                    </div>
+                    <input type="submit" value="login" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4"><br>
+                    <input type="reset" value="Annuler" class="flex-c-m size2 bg4 bo-rad-23 hov1 m-text3 trans-0-4">
+
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+</body>
+</html>
