7f6f2ce99312b11b73bbac8cb7073344f9665c67
Oceane.PICHONNAZ <Oceane.PICHONNAZ@SC-C232-PC08>
2021-12-03 11:58:34 +0100

    fonction écrire dans DB

--- model/userManagement.php
+++ model/userManagement.php
@@ -15,17 +15,18 @@ function addToDB($data){
 
     $firstname = $data['firstname'];
     $lastname = $data['lastname'];
-    $username = $data['username'];
+    $username = $data['pseudo'];
     $email = $data['email'];
-    $password = $data['password'];
-    $registration = new DateTime('now');
+    $pwd = $data['password'];
+    $registration = date('Y/m/d H:i:s');
     $status = 2;
     $chatroom = null;
 
 
-    $query="INSERT INTO users (firstname, lastname,username,email,registration_date,Chatroom_id,Users_states_id) 
-values (firstname=:ffn,lastname=:fln,username=:fun,email=:fem,registration_date=:rd,Chatroom_id=:chi,Users_states_id:=st) ";
-    $params = array(':ffn' => $firstname,':fln' => $lastname,':fun' => $username, ':fem' => $email, ':fpw' => $password,':rd'=>$registration,':chi'=>$chatroom,':st'=>$status);
+    $query="INSERT INTO users (firstname, lastname,username,password,email,registration_date,Chatroom_id,Users_states_id) values (firstname=:ffn,lastname=:fln,username=:fun,email=:fem,password=:fpw,registration_date=:rd,Chatroom_id=:chi,2) ";
+    $params = array(':ffn' => $firstname,':fln' => $lastname,':fun' => $username, ':fem' => $email, ':fpw' => $pwd,':rd'=>$registration,':chi'=>$chatroom);
+
+
 
     //open DB Connection
     $dbConnexion = openDBConnexion();
@@ -35,13 +36,15 @@ values (firstname=:ffn,lastname=:fln,username=:fun,email=:fem,registration_date=
         //preparation query
         $statement = $dbConnexion->prepare($query);
 
+
         //we execute the request with the parameters used on the query
         $statement -> execute($params);
 
-        $dbConnexion = null; // Fermeture de ma connection à la BD*/
-        return $dbConnexion->affected_rows;
+
+        return 1;
 
     }
+    $dbConnexion = null; // Fermeture de ma connection à la BD*/
 
-     return -1;
+     return 0;
 }
