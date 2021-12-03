fd71aa94baea071b10b9a25ace0f4e2dcf0e5b1d
Ba-Khanh-Henry.BURGA <ba-khanh-henry.burgat@cpnv.ch>
2021-12-03 10:41:42 +0100

    maincss responsive loginform

--- view/login.php
+++ view/login.php
@@ -11,29 +11,27 @@ ob_start();
 $title = "Login";
 
 ?>
-    <!-- content page -->
-<div class="container d-flex align-items-center">
-    <form id="formLogin" class="form col-10 col-xs-10 col-sm-8 col-md-6 col-lg-6 col-xl-6">
+<div>
+    <div class="container-fluid" id="formLogin">
+        <form>
+            <h3>Log in</h3>
 
-        <h3>Log in</h3>
+            <div class="form-outline mb-4">
+                <input type="email" class="form-control" id="email" name="email" placeholder="Email">
+            </div>
 
-        <div class="form-outline mb-4">
-            <input type="email" class="form-control" id="email" name="email" placeholder="Email">
-        </div>
+            <div class="form-outline mb-4">
+                <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Password">
+            </div>
 
-        <div class="form-outline mb-4">
-            <input type="password" class="form-control" id="pwd" name="pwd" placeholder="Password">
-        </div>
+            <div class="pt-1 mb-4">
+                <button class="btn btn-info btn-lg btn-block" type="submit">Login</button>
+            </div>
 
-        <div class="pt-1 mb-4">
-            <button class="btn btn-info btn-lg btn-block" type="submit">Login</button>
-        </div>
-
-        <p>Don't have an account? <a href="index.php?action=goToSignUp.php">Create an account</a></p>
-    </form>
+            <p>Don't have an account? <a href="index.php?action=goToSignUp.php">Create an account</a></p>
+        </form>
+    </div>
 </div>
-
-    <!---->
 
 <?php
 $content = ob_get_clean();
