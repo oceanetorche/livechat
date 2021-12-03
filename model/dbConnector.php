92d6fffc9bfdbcf15c33c5e1b1670aea6dab14ba
Oceane.PICHONNAZ <Oceane.PICHONNAZ@SC-C232-PC08>
2021-12-03 10:59:10 +0100

    minus infos

--- model/dbConnector.php
+++ model/dbConnector.php
@@ -11,7 +11,6 @@
 /**
  * @brief This function is designed to manage the database connexion. Closing will be not proceeded there. The client is responsible of this.
  * @return PDO|null
- * @remark : http://php.net/manual/en/pdo.construct.php
  */
 function openDBConnexion()
 {
@@ -33,50 +32,4 @@ function openDBConnexion()
 		die();
     }
     return $tempDBConnexion;
-}
-
-/**
- * @brief This function is designed to execute a query received as parameter
- * @param $query string the query to execute
- * @param $params array containing the named parameters of the query
- * @return array|null
- * @link https://php.net/manual/en/pdo.prepare.php
- */
-function executeQuerySelect($query, $params)
-{
-    $queryResult = null;
-
-    //open DB Connection
-    $dbConnexion = null;
-
-    //if connection is not null
-    if ($dbConnexion != null) {
-        //preparation query
-
-        //we execute the request with the parameters used on the query
-
-        //we prepare the results for the navigator
-
-
-    }
-    $dbConnexion = null; // Fermeture de ma connection à la BD
-    return $queryResult;
-}
-
-/**
- * @brief This function is designed to insert value in database
- * @param $query
- * @return null
- */
-function executeQueryInsert($query)
-{
-
-}
-
-
-
-// Classe pour gérer les exceptions liées au modèle
-class ModelDataException extends Exception
-{
-
 }
